56342aa4cc58e9d24a9564072c842972baa7e124
amel <mohaddese.computer@gmail.com>
2024-02-18 09:23:28 +0330

    add structure

---
+++ components/landing.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+landinfg
+</template>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+import Landing from "~/components/landing.vue";
+</script>
+
+<template>
+<landing/>
+</template>
+
+<style scoped>
+
+</style>
